5d489bcb6c9f6180bf5961baaf533db57f864b22
Umedul70 <technoumedul@gmail.com>
2021-11-17 23:35:10 +0530

    initial commit

--- StudentStudyPortal/settings.py
+++ StudentStudyPortal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f#n$*&nqf*9e1jfdv194u1+ugm*ziv6(zu60++4($@u_c81s%('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
